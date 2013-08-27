a140a7942fd4b38db99bf0a98969b760934c489b
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-08-26 20:47:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chirotech::Application.config.secret_token = '575ba90f4adca48650d7ca18f90a640d903921ea7b1af80f8d9e7dfe28dfc35a46b2e44d70fbba849e8f33dc08eb437668e777035d5030cabe465bf117885add'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chirotech::Application.config.session_store :cookie_store, key: '_chirotech_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chirotech::Application.config.session_store :active_record_store
